385eb24d569bd3766b0d978bdb6bfa990e679b1d
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-09-16 18:32:10 -0300

    Recupera id do álbum pela url

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../Components/Header/Header';
 
 class Album extends Component {
   render() {
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
     return (
       <div data-testid="page-album">
         <Header />
+        {console.log(id)}
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({ params: PropTypes.shape({ id: PropTypes.string }) })
+    .isRequired,
+};
 
 export default Album;
