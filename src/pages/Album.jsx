dff30c0624a15feaaf1898270e9a4f4038356455
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-09-16 20:08:16 -0300

    Mostra capa, nome do artista e no do álbum na página de album

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../Components/Header/Header';
+import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.fetchMusics = this.fetchMusics.bind(this);
+    this.state = {
+      musics: [],
+      artistName: '',
+      collectionName: '',
+      artworkUrl100: '',
+    };
+  }
+
+  componentDidMount() {
     const {
       match: {
         params: { id },
       },
     } = this.props;
+    this.fetchMusics(id);
+  }
+
+  async fetchMusics(id) {
+    const musics = await getMusics(id);
+    this.setState({
+      musics,
+      artistName: musics[0].artistName,
+      collectionName: musics[0].collectionName,
+      artworkUrl100: musics[0].artworkUrl100,
+    });
+  }
+
+  render() {
+    const { artistName, collectionName, artworkUrl100: cover } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        {console.log(id)}
+        <div>
+          <img src={ cover } alt="" />
+          <h2 data-testid="artist-name">{artistName}</h2>
+          <h3 data-testid="album-name">{collectionName}</h3>
+        </div>
       </div>
     );
   }
