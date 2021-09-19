d4d63b211305872338b6bd76971d9ab404b15985
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-09-19 20:59:50 -0300

    Implementa requisito 8

--- src/Components/MusicCard/MusicCard.jsx
+++ src/Components/MusicCard/MusicCard.jsx
@@ -1,23 +1,65 @@
+// Desenvolvido com a ajuda do Matheus Laurindo
+// PR: https://github.com/tryber/sd-014-b-project-trybetunes/pull/52
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { addSong, removeSong } from '../../services/favoriteSongsAPI';
+import {
+  addSong,
+  getFavoriteSongs,
+  removeSong,
+} from '../../services/favoriteSongsAPI';
+import Loading from '../Loading/Loading';
 
 class MusicCard extends Component {
   constructor(props) {
     super(props);
     this.handleFavorite = this.handleFavorite.bind(this);
+    this.recoverFavoriteSongs = this.recoverFavoriteSongs.bind(this);
+    this.state = {
+      checked: false,
+      display: 'inline',
+    };
+  }
+
+  componentDidMount() {
+    this.recoverFavoriteSongs();
   }
 
   async handleFavorite(event) {
+    const { checked } = event.target;
     const { music } = this.props;
-    const action = event.target.checked ? addSong : removeSong;
-    await action(music);
+    this.setState({ checked, display: 'none' });
+    if (checked) {
+      await addSong(music);
+    } else {
+      await removeSong(music);
+    }
+    this.setState({ display: 'inline' });
+  }
+
+  async recoverFavoriteSongs() {
+    const {
+      music: { trackId },
+    } = this.props;
+    const favoriteSongs = await getFavoriteSongs();
+    const checkFavoriteSong = favoriteSongs.some(
+      (music) => trackId === music.trackId,
+    );
+    if (checkFavoriteSong) {
+      this.setState({ checked: true });
+    }
   }
 
   render() {
     const {
-      music: { trackName, previewUrl, trackId },
+      music: { trackName, trackId, previewUrl },
     } = this.props;
+
+    const { checked, display } = this.state;
+
+    if (trackName === undefined) {
+      return null;
+    }
+
     return (
       <div>
         <p>{trackName}</p>
@@ -27,15 +69,16 @@ class MusicCard extends Component {
           <code>audio</code>
           .
         </audio>
-        <label htmlFor={ trackId } data-testid={ `checkbox-music-${trackId}` }>
-          Favorita
+        <label htmlFor={ `${trackId}` } data-testid={ `checkbox-music-${trackId}` }>
           <input
-            id={ trackId }
             type="checkbox"
-            onChange={ (event) => {
-              this.handleFavorite(event);
-            } }
+            id={ `${trackId}` }
+            name="checked"
+            checked={ checked }
+            onChange={ this.handleFavorite }
+            style={ { display } }
           />
+          {display === 'none' ? <Loading /> : null}
         </label>
       </div>
     );
@@ -45,8 +88,8 @@ class MusicCard extends Component {
 MusicCard.propTypes = {
   music: PropTypes.shape({
     trackName: PropTypes.string,
-    previewUrl: PropTypes.string,
     trackId: PropTypes.number,
+    previewUrl: PropTypes.string,
   }).isRequired,
 };
 
